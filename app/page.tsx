a14ab9104bbdcbf3db0fea106c23941d927eaee0
devvoy7 <devvoy7@gmail.com>
2024-12-27 10:48:33 +0000

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-
+import { Navbar } from "@/components/navbar";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2xl font-bold">This is a Iphone Landing Page</h1>
-    </main>
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className="text-2xl font-bold">This is a Iphone Landing Page</h1>
+      </main>
+    </>
   );
 }
